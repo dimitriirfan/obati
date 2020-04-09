0ca12c194083ecfd7ca2a7b2dafbc379d746963f
wira-sigalingging <43566126+wira-sigalingging@users.noreply.github.com>
2020-04-09 14:16:54 +0700

    Add files via upload

---
+++ application/controllers/Controller_cart.php
@@ -0,0 +1,12 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Controller_cart extends CI_Controller {
+
+
+	public function index()
+	{
+		$this->load->view('view_cart');
+	}
+
+}
