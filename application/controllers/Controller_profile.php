aa8ab9e35fc07c2c524d36e1ccf6d25995f91e50
Clysens <fazainsyah@gmail.com>
2020-04-17 21:05:31 +0700

    add profile page

--- application/controllers/Controller_profile.php
+++ application/controllers/Controller_profile.php
@@ -6,7 +6,7 @@ class Controller_profile extends CI_Controller {
 
 	public function index()
 	{
-        $data['title'] = 'Edit Profile';
+        $data['title'] = 'Profile';
 		$this->load->view('template/header', $data);
 		$this->load->view('view_editprofile');
 		$this->load->view('template/footer');
--- application/views/view_profile.php
+++ application/views/view_profile.php
@@ -10,7 +10,7 @@
 	</link>
 	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" 
         integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-    <link rel="stylesheet" href="<?php echo base_url("./assets/css/editprofile.css")?>" media="screen" title="no title" charset="utf-8">
+    <link rel="stylesheet" href="<?php echo base_url("./assets/css/profile.css")?>" media="screen" title="no title" charset="utf-8">
    
 </head>
 <body>
