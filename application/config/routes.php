3e44b54e9c2c54ec4a5e7fb0ba44cb1321d05c97
dimitriirfan <dimitriirfan@gmail.com>
2020-04-20 18:31:12 +0700

    fixed conflict

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,9 +49,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-<<<<<<< HEAD
 $route['default_controller'] = 'Controller_landing';
-=======
->>>>>>> gilang
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
