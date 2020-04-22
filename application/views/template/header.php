ca13ba17b432ffd05898ea99122329f783471178
wira-sigalingging <43566126+wira-sigalingging@users.noreply.github.com>
2020-04-22 15:55:04 +0700

    Add files via upload

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -4,7 +4,7 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="icon" href="<?php echo base_url("./obati.ico")?>" >
+	<link rel="icon" href="<?php echo base_url("./obati.ico")?>">
 
 	<!-- bootstrap -->
 	<link rel="stylesheet" href="<?php echo base_url("./assets/css/bootstrap/bootstrap.min.css") ?>">
@@ -14,6 +14,27 @@
 
 	<!-- style -->
 	<link rel="stylesheet" href="<?php echo base_url("./assets/css/$title.css")?>">
+
+	<?php if ($title != "register" && $title != "login") :?>
+	<link rel="stylesheet" href="<?php echo base_url("./assets/css/navbar.css")?>">
+	<?php endif?>
+
+	<?php if ($title == 'article') : ?>
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url ("./assets/css/article/owl.carousel.css") ?>"
+		rel="stylesheet" />
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url ("./assets/css/article/owl.theme.default.css") ?>"
+		rel="stylesheet" type="text/css" />
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url ("./assets/css/article/media_query.css") ?>"
+		rel="stylesheet" type="text/css" />
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url ("./assets/css/article/animate.css") ?>"
+		rel="stylesheet" type="text/css" />
+
+	<?php endif?>
+
+	<?php if ($title == 'cart') : ?>
+	<link rel="stylesheet" href="<?php echo base_url("./assets/css/cart_responsive.css")?>">
+	<?php endif?>
+
 
 
 	<title>Obati | <?= $title?></title>
